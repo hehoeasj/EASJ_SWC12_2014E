8e68d2ec436e1b1b0d319e945df94090710dcf4c
heho <heho@easj.dk>
2014-10-27 14:51:00 +0100

    nyt

---
+++ Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class Test
+    {
+        public string test { get; set; }    
+ 
+    }
+}
